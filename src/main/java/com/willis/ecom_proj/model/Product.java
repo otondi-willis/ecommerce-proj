4a6ddf74c2c5c4a016e54dfdd6794c498bb18b65
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-01 15:38:47 +0300

    product attributes created

---
+++ src/main/java/com/willis/ecom_proj/model/Product.java
@@ -0,0 +1,20 @@
+package com.willis.ecom_proj.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean isAvailable;
+    private int quantity;
+
+
+}
