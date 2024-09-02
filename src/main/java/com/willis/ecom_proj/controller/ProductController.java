373a2ffdca5f5660b91380e338d44a1b381e1790
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 06:08:53 +0300

    added annotation

--- src/main/java/com/willis/ecom_proj/controller/ProductController.java
+++ src/main/java/com/willis/ecom_proj/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.willis.ecom_proj.controller;
 
 import com.willis.ecom_proj.model.Product;
 import com.willis.ecom_proj.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
+    @Autowired
     private ProductService service;
     @RequestMapping("/")
     public String greet(){
