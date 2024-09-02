c9c82b388a6a632e29086e3029a14b0b91ac45da
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 05:17:36 +0300

    created a method to return all products

--- src/main/java/com/willis/ecom_proj/controller/ProductController.java
+++ src/main/java/com/willis/ecom_proj/controller/ProductController.java
@@ -1,13 +1,23 @@
 package com.willis.ecom_proj.controller;
 
+import com.willis.ecom_proj.model.Product;
+import com.willis.ecom_proj.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+    private ProductService service;
     @RequestMapping("/")
     public String greet(){
         return "hello world";
+    }
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
     }
 }
