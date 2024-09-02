2a343bf95fe6fa1b9fdb0b4c5b668f0ad206d3a8
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 08:57:25 +0300

    changing the date format

--- src/main/java/com/willis/ecom_proj/model/Product.java
+++ src/main/java/com/willis/ecom_proj/model/Product.java
@@ -1,5 +1,6 @@
 package com.willis.ecom_proj.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +26,7 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-mm-yyyy")
     private Date releaseDate;
     private boolean isAvailable;
     private int quantity;
