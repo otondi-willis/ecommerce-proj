c34ef0ebfae8f14397a4a02ac2b356bdd5ee98bd
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 05:17:04 +0300

    created service layer

---
+++ src/main/java/com/willis/ecom_proj/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.willis.ecom_proj.service;
+
+import com.willis.ecom_proj.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    public List<Product> getAllProducts() {
+    }
+}
