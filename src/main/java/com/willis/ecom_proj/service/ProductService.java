dbcbdeec79c7fd45fe065cd8a7b22e00bbb67b18
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 05:24:07 +0300

    autowire repository layer to service layer

--- src/main/java/com/willis/ecom_proj/service/ProductService.java
+++ src/main/java/com/willis/ecom_proj/service/ProductService.java
@@ -1,12 +1,16 @@
 package com.willis.ecom_proj.service;
 
 import com.willis.ecom_proj.model.Product;
+import com.willis.ecom_proj.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
+    @Autowired
+    private ProductRepository repository;
     public List<Product> getAllProducts() {
     }
 }
