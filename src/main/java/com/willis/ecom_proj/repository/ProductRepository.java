f5c1d4ec63766c4398805d45dd1d6f181afa8476
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 05:22:23 +0300

    created a repository layer that extends JpaRepository

--- src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
+++ src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.willis.ecom_proj.repository;
 
+import com.willis.ecom_proj.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
