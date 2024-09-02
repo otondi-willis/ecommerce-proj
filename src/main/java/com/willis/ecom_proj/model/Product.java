5a22bd26039b70e0ae93022a123dff879a2b4a6a
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-02 05:43:57 +0300

    use of @GeneratedValue annotation

--- src/main/java/com/willis/ecom_proj/model/Product.java
+++ src/main/java/com/willis/ecom_proj/model/Product.java
@@ -1,6 +1,8 @@
 package com.willis.ecom_proj.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +18,7 @@ import java.util.Date;
 
 public class Product {
     @Id
-
+@GeneratedValue(strategy = GenerationType.IDENTITY )
     private int id;
     private String name;
     private String description;
