e3f888162810304dc46f2738fe7681defe8c8061
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-03 05:40:02 +0300

    removed the date format

--- src/main/java/com/willis/ecom_proj/controller/ProductController.java
+++ src/main/java/com/willis/ecom_proj/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.willis.ecom_proj.controller;
 import com.willis.ecom_proj.model.Product;
 import com.willis.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +20,15 @@ public class ProductController {
         return "hello world";
     }
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
     @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable int id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProduct(@PathVariable int id){
+        Product product=service.getProductById(id);
+        if(product != null)
+        return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
