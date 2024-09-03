b47b9fc5322fb8dee67354879f279d6f0c72a48f
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-03 09:15:58 +0300

    fetching image file

--- src/main/java/com/willis/ecom_proj/service/ProductService.java
+++ src/main/java/com/willis/ecom_proj/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,7 +21,10 @@ public class ProductService {
         return repository.findById(id).orElse(new Product());
     }
 
-    public Product addProduct(Product product, MultipartFile imageFile) {
-     repository.save(product);
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repository.save(product);
     }
 }
