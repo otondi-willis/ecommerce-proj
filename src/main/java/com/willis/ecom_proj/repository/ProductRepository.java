63a21c5b6f0f924f72d3e09985b1ab5a352cb2ed
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-04 21:46:41 +0300

    @Query annotation

--- src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
+++ src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.willis.ecom_proj.repository;
 
 import com.willis.ecom_proj.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     //JPQL- same as SQL in SQL we use tables,column names here we use class names, field names
+    @Query
     List<Product> searchProducts(String keyword);
 }
