780f71cf0812c613608ca6a532acbdb713bb4f65
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-04 22:13:51 +0300

    search functionality pass to the repo

--- src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
+++ src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     //JPQL- same as SQL in SQL we use tables,column names here we use class names, field names
-    @Query()
+    @Query("SELECT p from Product p WHERE "+"LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR" +
+            " " + "SELECT p from Product p WHERE "+"LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR"
+    + "SELECT p from Product p WHERE "+"LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR"
+    +"SELECT p from Product p WHERE "+"LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Product> searchProducts(String keyword);
 }
