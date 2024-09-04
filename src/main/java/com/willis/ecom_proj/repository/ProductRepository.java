15f16c82d575c66db2adb362394a3a1f9a9b6964
otondi-willis <willis.otondi.ke@gmail.com>
2024-09-04 21:46:01 +0300

    JPQL introduced

--- src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
+++ src/main/java/com/willis/ecom_proj/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import com.willis.ecom_proj.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    //JPQL- same as SQL in SQL we use tables,column names here we use class names, field names
+    List<Product> searchProducts(String keyword);
 }
